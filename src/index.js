f4cca8c70902c0fbfc5596a43f4fae605dec265d
Ana  Buon <anabuon@icloud.com>
2021-03-11 14:28:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    wNum = n * -1;
+    num = +wNum.toString().split("").reverse().join("");
+    num1 = +n.toString().split("").reverse().join("");
+    return n < 0 ? num : num1;
+};
